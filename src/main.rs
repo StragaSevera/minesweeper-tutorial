217d2d2bf9c29a6430d6717566334eed9f08e38a
StragaSevera <ought@yandex.ru>
2022-05-16 20:00:18 +0300

    Add visual board

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
-
 #[cfg(feature = "debug")]
 use bevy_inspector_egui::WorldInspectorPlugin;
+
+use board_plugin::resources::BoardOptions;
 use board_plugin::BoardPlugin;
 
 fn main() {
@@ -12,12 +13,20 @@ fn main() {
         width: 700.,
         height: 800.,
         ..Default::default()
-    })
-    .add_plugins(DefaultPlugins)
-    .add_plugin(BoardPlugin);
+    });
+    app.add_plugins(DefaultPlugins);
 
     #[cfg(feature = "debug")]
     app.add_plugin(WorldInspectorPlugin::new());
+
+    // Board plugin options
+    app.insert_resource(BoardOptions {
+        map_size: (20, 20),
+        bomb_count: 40,
+        tile_padding: 3.0,
+        ..Default::default()
+    });
+    app.add_plugin(BoardPlugin);
 
     app.add_startup_system(camera_setup);
     app.run();
